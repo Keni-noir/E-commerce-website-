6e33c16a39ddce28ee1c956431dab2441844f6d5
Keni-noir <keninoir03@example.com>
2026-03-04 13:07:48 +0000

    lib/stripe updated

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,7 @@
-import Stripe from "stripe";
+import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+if (!process.env.STRIPE_SECRET_KEY) {
+    throw new Error("STRIPE_SECRET_KEY is not set in environment variables")
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
